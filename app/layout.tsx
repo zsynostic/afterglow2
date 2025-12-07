786568537e449897005c50194abd5121ae4060d1
Zsynostic <zsynosticalist@gmail.com>
2025-12-07 16:46:46 +0700

    Fix metadata pointing to deleted icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,23 +15,10 @@ export const metadata: Metadata = {
     "Khám phá về sự thiếu hiểu biết trong xã hội hiện đại và tìm kiếm những phương pháp hiệu quả để thay đổi",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/icon.png",
+    apple: "/icon.png",
   },
-}
+};
 
 export default function RootLayout({
   children,
